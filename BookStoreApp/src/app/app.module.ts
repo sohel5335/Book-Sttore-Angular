39f59bfc3198d8c10d75a5d33f5b5e227d9559bb
sohel5335 <rsohel5335@gmail.com>
2022-10-30 00:53:00 +0600

    app init

--- BookStoreApp/src/app/app.module.ts
+++ BookStoreApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HowItWorksComponent
   ],
   imports: [
     BrowserModule
